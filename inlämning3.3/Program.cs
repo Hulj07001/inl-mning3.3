61fe2793ab1fdf4e58c94ffe8a9457079c0a55c6
Hugo Ljungqvist <hulj07001@utb.vaxjo.se>
2024-11-13 12:17:06 +0100

    Add project files.

---
+++ inlämning3.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace inlämning3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej! Välkommen till vår bilhyrfirma.");
+            Console.WriteLine("En hyrbil kostar 80kr i timmen.");
+            Console.WriteLine("Hur många timmar vill du hyra en bil idag? OBS! Endast hela timmar.");
+            int timmar = int.Parse(Console.ReadLine());
+            int kostnad = timmar * 80;
+            Console.WriteLine("För att hyra en bil i "+timmar+" timmar kommer det att kosta: "+kostnad+"kr");
+        }
+    }
+}
